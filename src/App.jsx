8b4b8c4f9ccb9edfe25f920e5903b7e596406a2b
dan9b6 <dkburton0@gmail.com>
2020-03-02 14:08:31 +0000

    added main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 import './App.css';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <main>
+          <div className="Main-page">
+            <h1>
+              Say hello to <br />
+              ReactJS
+            </h1>
+            <p>You will learn a frontend framework from scratch, to become a Ninja Developer.</p>
+            <button>Awesome!</button>
+          </div>
+          <div className="Options">
+            <div className="Row">
+              <div className="Box"></div>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="App-header">
+      <img src="images/ironhack-logo.svg" alt="logo" />
+      <img src="images/menu-top.svg" alt="Menu" />
+    </nav>
+  );
+};
+
+export default Nav;
